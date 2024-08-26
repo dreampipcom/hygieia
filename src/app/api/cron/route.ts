c95e04a57d7d8c6e41850c790a225996b885be3b
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-26 19:58:31 +0100

    ar(fix) missing var

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import * as Sentry from "@sentry/nextjs";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 async function fetchWithTimeout(resource: string, options: any) {
   const { timeout = 10000 } = options;
