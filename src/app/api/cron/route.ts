ca099b8bb1e79eb099bb29e9fcb258c6a1eded81
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-26 18:37:57 +0100

    ar(install) sentry

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,8 +3,6 @@
 import { NextResponse } from "next/server";
 import * as Sentry from "@sentry/nextjs";
 
-export const fetchCache = 'force-no-store;'
-
 async function fetchWithTimeout(resource: string, options: any) {
   const { timeout = 10000 } = options;
 
