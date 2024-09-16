281d30f9e66969d6af464892c951cfd1885cad62
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-09-16 19:17:41 +0100

    ar(fix) [DPTM-14]: disable kv

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,14 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { next } from "@vercel/edge";
-import { ipAddress } from "@vercel/functions";
-import { kv } from '@vercel/kv';
-import { Ratelimit } from '@upstash/ratelimit';
+// import { ipAddress } from "@vercel/functions";
+// import { kv } from '@vercel/kv';
+// import { Ratelimit } from '@upstash/ratelimit';
 
-const ratelimit = new Ratelimit({
-  redis: kv,
-  limiter: Ratelimit.slidingWindow(10, '3 s'),
-});
+// const ratelimit = new Ratelimit({
+//   redis: kv,
+//   limiter: Ratelimit.slidingWindow(10, '3 s'),
+// });
 
 export const config = {
   matcher: ["/api/:path*"],
@@ -42,9 +42,9 @@ export default async function middleware(request: NextRequest) {
 
   // You could alternatively limit based on user ID or similar
   const response = next();
-  const ip = ipAddress(request) || "127.0.0.1";
+  // const ip = ipAddress(request) || "127.0.0.1";
 
-  const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
+  // const { success, pending, limit, reset, remaining } = await ratelimit.limit(ip);
 
   if (!request?.url?.includes("auth")) {
     Object.keys(headers).forEach((key: string) => {
@@ -52,10 +52,10 @@ export default async function middleware(request: NextRequest) {
     });
   }
 
-  return success ? response : NextResponse.redirect(new URL('https://www.dreampip.com/404', request.url));
-  // return response
-  //   ? response
-  //   : NextResponse.redirect(
-  //       new URL("https://www.dreampip.com/404", request.url),
-  //     );
+  // return success ? response : NextResponse.redirect(new URL('https://www.dreampip.com/404', request.url));
+  return response
+    ? response
+    : NextResponse.redirect(
+        new URL("https://www.dreampip.com/404", request.url),
+      );
 }
